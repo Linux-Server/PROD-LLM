884cef4b290a4d6a327200076425aa34824fa740
Sachin <rollercoster246@gmail.com>
2026-04-27 14:20:30 +0530

    api created

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 # 1. Setup the virtual env
 # 2. Install , fastapi
 # 3. Set github and commit
+# 4. Create fastapi server
 
 
 
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from prod-llm!")
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.post("/predict")   
+async def predict(data: dict):
+    return {"prediction": data["name"]}
